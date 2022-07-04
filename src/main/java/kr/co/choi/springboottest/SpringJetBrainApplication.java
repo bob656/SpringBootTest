4bdb9c39a46f1f8083ce9216d99de78435e45d1e
bob656 <pm665@naver.com>
2022-07-04 17:13:16 +0900

    Initial commit

---
+++ src/main/java/kr/co/choi/springboottest/SpringJetBrainApplication.java
@@ -0,0 +1,12 @@
+package kr.co.choi.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJetBrainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJetBrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/choi/springboottest/SpringJetBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.choi.springboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJetBrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
