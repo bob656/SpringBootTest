dc780fce28ddb229ba2ae85104ff8d62359e7a22
bob656 <pm665@naver.com>
2022-07-05 09:18:51 +0900

    Changes

--- src/main/java/kr/co/choi/springboottest/SpringJetBrainApplication.java
+++ src/main/java/kr/co/choi/springboottest/SpringJetBrainApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJetBrainApplication {
+
+
     public static void main(String[] args) {
         SpringApplication.run(SpringJetBrainApplication.class, args);
     }
+
+
 
 }
