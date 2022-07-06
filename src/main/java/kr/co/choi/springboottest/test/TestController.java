b28bac16a839ede4b0ca3a3092b5252a9682d6bd
bob656 <pm665@naver.com>
2022-07-06 15:43:09 +0900

    Changes

--- src/main/java/kr/co/choi/springboottest/test/TestController.java
+++ src/main/java/kr/co/choi/springboottest/test/TestController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.log4j.Log4j2;
 
@@ -12,9 +13,9 @@ import lombok.extern.log4j.Log4j2;
 public class TestController {
 
     @RequestMapping(value="parameter", method= RequestMethod.GET)
-    public ResponseEntity<String> getParameterTest(){
+    public ResponseEntity<String> getParameterTest(@RequestParam(value="msg")String msg){
         log.debug("안녕하세요");
-        return new ResponseEntity<>("안녕하세요", HttpStatus.OK);
+        return new ResponseEntity<>(msg, HttpStatus.OK);
 
     }
 
